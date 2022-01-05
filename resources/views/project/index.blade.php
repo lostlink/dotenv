9617992d353ad475d8cc3e4e07d6ecb7ffa49847
Nuno Souto <nsouto@lostlink.net>
2022-01-04 22:02:55 -0500

    Proper Buttons for Project Index and remove left-over code
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -86,11 +86,28 @@
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                         <a href="{{ route('project.show', ['project' => $project->routeKey]) }}"
-                                           class="text-indigo-600 hover:text-indigo-900">View</a>
+                                           class="text-indigo-600 hover:text-indigo-900">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                                <x-heroicon-s-eye class="-ml-0.5 mr-2 h-4 w-4"/>
+                                                View
+                                            </button>
+                                        </a>
+
                                         <a onclick='Livewire.emit("openModal", "project.edit", {!! json_encode(['project' => $project->id]) !!})'
-                                           class="cursor-pointer text-green-600 hover:text-green-900">Edit</a>
+                                           class="cursor-pointer text-green-600 hover:text-green-900">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                                                <x-heroicon-s-pencil class="-ml-0.5 mr-2 h-4 w-4"/>
+                                                Edit
+                                            </button>
+                                        </a>
+
                                         <a onclick='Livewire.emit("openModal", "project.delete", {!! json_encode(['project' => $project->id]) !!})'
-                                           class="cursor-pointer text-red-600 hover:text-red-900">Delete</a>
+                                           class="cursor-pointer text-red-600 hover:text-red-900">
+                                            <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                                <x-heroicon-s-trash class="-ml-0.5 mr-2 h-4 w-4"/>
+                                                Delete
+                                            </button>
+                                        </a>
                                     </td>
                                 </tr>
                             @endforeach
@@ -99,67 +116,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-
-
-        <div class="bg-white shadow overflow-hidden sm:rounded-md">
-            <ul role="list" class="divide-y divide-gray-200">
-
-                @foreach($projects as $project)
-                    <li>
-                        <a href="{{ route('project.show', ['project' => $project->routeKey]) }}"
-                           class="block hover:bg-gray-50">
-                            <div class="px-4 py-4 sm:px-6">
-                                <div class="flex items-center justify-between">
-                                    <p class="text-sm font-medium text-indigo-600 truncate">
-                                        {{ $project->name }}
-                                    </p>
-
-                                    <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
-                                        <!-- Heroicon name: solid/calendar -->
-                                        <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                             aria-hidden="true">
-                                            <path fill-rule="evenodd"
-                                                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                                  clip-rule="evenodd"/>
-                                        </svg>
-                                        <p>
-                                            Closing on
-                                            <time datetime="2020-01-14">January 14, 2020</time>
-                                        </p>
-                                    </div>
-                                </div>
-                                <div class="mt-2 sm:flex sm:justify-between">
-                                    <div class="sm:flex">
-                                        <p class="flex items-center text-sm text-gray-500">
-                                            <x-heroicon-s-location-marker
-                                                class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"/>
-                                            Targets:
-                                        </p>
-                                        @foreach($project->targets as $target)
-                                            <div class="ml-2 flex-shrink-0 flex">
-                                                <p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full {{ \App\Helpers\BadgeColor::get($target->color) ?? \App\Helpers\BadgeColor::guess($target->name) }}">
-                                                    {{ $target->name }}
-                                                </p>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                    {{--                                    <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">--}}
-                                    {{--                                        <x-heroicon-s-calendar class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"/>--}}
-                                    {{--                                        <p>--}}
-                                    {{--                                            Updated--}}
-                                    {{--                                            <time datetime="{{ $project->updated_at->format('Y-M-D') }}">--}}
-                                    {{--                                                {{ $project->updated_at->format('M d, Y') }}--}}
-                                    {{--                                            </time>--}}
-                                    {{--                                        </p>--}}
-                                    {{--                                    </div>--}}
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
         </div>
     </div>
 
