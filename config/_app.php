82e6bf38dd691c5023556e2e16a3a86daf08260d
Nuno Souto <nsouto@lostlink.net>
2022-01-21 10:00:46 -0500

    Update GHA to PHP 8.1 remove notice from using trim(null)
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- config/_app.php
+++ config/_app.php
@@ -6,15 +6,15 @@ return [
     ],
 
     'binaries' => [
-        'npm' => env('NPM_BINARY_PATH', trim(shell_exec('which npm'))),
-        'node' => env('NODE_BINARY_PATH', trim(shell_exec('which node'))),
+        'npm' => env('NPM_BINARY_PATH', trim(shell_exec('which npm') ?? '')),
+        'node' => env('NODE_BINARY_PATH', trim(shell_exec('which node') ?? '')),
     ],
 
     'paths' => [
         'node_modules' => env(
             'NODE_MODULES_PATH',
             collect([
-                trim(shell_exec('npm list -g | head -1')),
+                trim(shell_exec('npm list -g | head -1') ?? ''),
                 'node_modules',
             ])->implode(DIRECTORY_SEPARATOR)
         ),
