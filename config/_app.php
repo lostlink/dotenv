a71fdde4a9fa7c4569b603df7ae913a3d7d68302
Nuno Souto <nsouto@lostlink.net>
2022-01-26 08:02:02 -0500

    Config Chromium Binary Path
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- config/_app.php
+++ config/_app.php
@@ -8,6 +8,13 @@ return [
     'binaries' => [
         'npm' => env('NPM_BINARY_PATH', trim(shell_exec('which npm') ?? '')),
         'node' => env('NODE_BINARY_PATH', trim(shell_exec('which node') ?? '')),
+        'chrome' => env(
+            'CHROME_BINARY_PATH',
+            collect([
+                trim(shell_exec('npm list -g | head -1') ?? ''),
+                'node_modules/puppeteer/.local-chromium/linux-950341/chrome-linux',
+            ])->implode(DIRECTORY_SEPARATOR)
+        ),
     ],
 
     'paths' => [
