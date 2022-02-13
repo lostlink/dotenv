0f04a90664edf6244d1d326ae592ac8469b1dfbd
Nuno Souto <nsouto@lostlink.net>
2022-02-13 15:33:52 -0500

    Remove button on screenshot thumbnail
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- resources/views/screenshot/livewire/show.blade.php
+++ resources/views/screenshot/livewire/show.blade.php
@@ -4,13 +4,12 @@
 
     <div
         class="absolute bottom-0 right-0 transform translate-y-1/2 translate-x-1/2 block border-2 border-white rounded-full">
-        <button type="button"
-                onclick='Livewire.emit("openModal", "screenshot.update", @json(['class'=> $model::class, 'model' => $model]))'
-                class="cursor-pointer block h-5 w-5 rounded-full bg-gray-300 justify-center">
-            <x-heroicon-s-camera
-                class="h-4 w-4 text-indigo-500 ml-0.5 hover:text-indigo-700"/>
-        </button>
-
+        {{--<button type="button"--}}
+        {{--        onclick='Livewire.emit("openModal", "screenshot.update", @json(['class'=> $model::class, 'model' => $model]))'--}}
+        {{--        class="cursor-pointer block h-5 w-5 rounded-full bg-gray-300 justify-center">--}}
+        {{--    <x-heroicon-s-camera--}}
+        {{--        class="h-4 w-4 text-indigo-500 ml-0.5 hover:text-indigo-700"/>--}}
+        {{--</button>--}}
     </div>
 
 
